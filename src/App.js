e0059547f037ea9367a434b2f37244dd70c82bd2
Michał Cendrowski <micend123@gmail.com>
2022-04-29 20:14:37 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const initialState = {
+  text: '',
+  words: 0
+}
+
 function App() {
+  const [leftTextArea, setLeftTextArea] = useState(initialState);
+  const [rightTextArea, setRightTextArea] = useState(initialState);
+
+  const countWords = (text) => {
+    const trimmedText = text.trim();
+    if(!trimmedText)
+      return 0;
+
+    return trimmedText.split(' ').length;
+  }
+
+  const onChangeLeftTextArea = (e) =>{
+    const text = e.target.value;
+    const words = countWords(text);
+    setLeftTextArea({text, words})
+  }  
+  
+  const onChangeRightTextArea = (e) =>{
+    const text = e.target.value;
+    const words = countWords(text);
+    setRightTextArea({text, words})
+  }
+
+  const areEqual = leftTextArea.text === rightTextArea.text;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        value={leftTextArea.text}
+        onChange={onChangeLeftTextArea}
+      />
+      {areEqual && <label className="sign">equal</label>}
+      {!areEqual && <label className="sign">not equal</label>}
+      <textarea
+        value={rightTextArea.text}
+        onChange={onChangeRightTextArea}
+      />
+      <div />
+      <label className="count">Word count: {leftTextArea.words}</label>
+      <label className="count">Word count: {rightTextArea.words}</label>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<App />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
+// here should be more unit tests but I'm running out of time
